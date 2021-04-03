65dd50f3d4ac5537a5668facb485970793fbc3b2
jlngllt <julien.guillot01@gmail.com>
2021-04-03 21:59:55 +0000

    at least it compile

--- weqj.c
+++ weqj.c
@@ -4,9 +4,10 @@
 #  define _XOPEN_SOURCE 600
 #endif
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_NB_SUBS 32
 
@@ -25,31 +26,34 @@ extract_subs(char *domain, char *subs[]) {
         subs[ntokens] = tok;
         ptr = save;
     }
-    return ntokens
+    return ntokens;
 }
 
 static void
 usage(FILE *f) {
-    fprint(f, "Usage: weqj [-vh] [-w WORDS] domains")
+    fprintf(f, "Usage: weqj [-vh] [-w WORDS] [-d DOMAINS]\n");
 }
 
 int main(int argc, char **argv) {
     char domain[] = "www.sub.blog.admin.domain.tld";
-    /*char *words = "toto\ntiti";*/
     char *subs[MAX_NB_SUBS];
 
-    int option
+    int option;
     while ((option = getopt(argc, argv, "d:w:h")) != -1) {
-        swtich(option) {
-        case d: {
-                    int nsubs = get_subs(domain, subs);
+        switch(option) {
+            case 'd': {
+                          int nsubs = extract_subs(domain, subs);
+                          int ii = 0;
+                          printf("nsubs = %d\n", nsubs);
+                          for (ii = 0; ii < nsubs; ++ii) {
+                            printf("> %s\n", subs[ii]);
+                          }
+                      } break;
+            case 'h': {
+                          usage(stdout);
+                          exit(EXIT_SUCCESS);
+                      } break;
         }
-        case 'h':
-            usage(stdout);
-            exit(EXIT_SUCCESS);
-        break;
     }
-
-
     return 0;
 }
