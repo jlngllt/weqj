b9d560116f36cf413988c1970eaa9bf36a0db4e3
jlngllt <julien.guillot01@gmail.com>
2021-04-04 22:24:30 +0000

    WIP read from stdin 2

--- weqj.c
+++ weqj.c
@@ -5,10 +5,13 @@
 #endif
 
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#define WEQJ_NUM_ELEM(x) (sizeof(x) / sizeof(*x))
 
 #define WEQJ_VERSION 0.0.1
 
@@ -21,9 +24,7 @@
 #define WEQJ_MAX_BYTES 1024 * 100
 static char io_buf[WEQJ_MAX_BYTES];
 
-static void print_version(void) {
-    puts("weqj " XSTR(WEQJ_VERSION));
-}
+static void print_version(void) { puts("weqj " XSTR(WEQJ_VERSION)); }
 
 static int extract_token(char *in, char *delim, char *out[], int count) {
   int ntokens = 0;
@@ -60,22 +61,18 @@ static long extract_words(char *path, char *words[]) {
 }
 
 static void free_words(int n, char *words[]) {
-    int ii;
-    for (ii = 0; ii < n; ++ii) {
-        free(words[ii]);
-    }
+  int ii;
+  for (ii = 0; ii < n; ++ii) {
+    free(words[ii]);
+  }
 }
 
-static int read_stdin(char *domains[], int count) {
-    int tokenno, n, ii;
-    n = read(STDIN_FILENO, (char *)io_buf, sizeof(io_buf));
-    if (n <= 0) 
-        return n;
-    tokenno = extract_token(io_buf, "\n", domains, count);
-    for (ii = 0; ii < tokenno; ii++) {
-        printf("domains %d = %s\n", ii, domains[ii]);
-    }
-    return tokenno;
+static int read_line_from_stdin(char *domains[], int count) {
+  int tokenno, n, ii;
+  n = read(STDIN_FILENO, (char *)io_buf, sizeof(io_buf));
+  if (n <= 0)
+    return n;
+  return extract_token(io_buf, "\n", domains, count);
 }
 
 static void print_usage(FILE *f) {
@@ -88,18 +85,8 @@ int main(int argc, char **argv) {
   char *file;
   int option, nwords, nsubs, ii;
   nwords = 0;
-  while ((option = getopt(argc, argv, "d:w:hv")) != -1) {
+  while ((option = getopt(argc, argv, "w:hv")) != -1) {
     switch (option) {
-    #if 0
-    case 'd': {
-      int ii;
-      nsubs = extract_subs(optarg, subs);
-      printf("nsubs = %d\n", nsubs);
-      for (ii = 0; ii < nsubs; ++ii) {
-        printf("> %s\n", subs[ii]);
-      }
-    } break;
-    #endif
     case 'w': {
       nwords = extract_words(optarg, words);
       printf("nwords = %d\n", nwords);
@@ -116,25 +103,41 @@ int main(int argc, char **argv) {
       exit(EXIT_SUCCESS);
     } break;
     default: {
-                 print_usage(stderr);
-                 exit(EXIT_FAILURE);
+      print_usage(stderr);
+      exit(EXIT_FAILURE);
     }
     }
   }
 
-  {
-  char *domains[256];
-  int domainsno;
-  /* FIXME: max 256 */
-  /*
-  for (ii = optind; ii < argc; ii++) {
-      if (argv[ii] == '-') {
-        domainsno += read_stdin(domains);
-      } 
-      domains[domainsno] = strdup(argv[ii])
-  }
-  */
-  domainsno += read_stdin(domains, 256);
+  { /* test */
+    char *domains[256];
+    const int max_domains = WEQJ_NUM_ELEM(domains);
+    int domainsno, n;
+    bool read_from_stdin = false;
+    if (optind < argc) {
+      domainsno = 0;
+      for (ii = optind; domainsno < max_domains, ii < argc; ii++) {
+        if (argv[ii][0] == '-' && argv[ii][1] == '\0') {
+          if (!read_from_stdin) {
+            n = read_line_from_stdin(domains, max_domains - domainsno);
+            domainsno += n;
+            read_from_stdin = true;
+          }
+        } else {
+          domains[domainsno] = strdup(argv[ii]);
+          domainsno++;
+        }
+      }
+    } else if (!isatty(STDIN_FILENO)) {
+      domainsno = read_line_from_stdin(domains, max_domains);
+    } else {
+      fprintf(stderr, "domains are missing\n");
+      print_usage(stderr);
+      exit(EXIT_FAILURE);
+    }
+    for (ii = 0; ii < domainsno; ii++) {
+        printf("%s\n", domains[ii]);
+    }
   }
   free_words(nwords, words);
   return 0;
